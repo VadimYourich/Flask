b0d1a31c1f2c4ca91f7d2fbe9f1683cfd010f3f7
vadim <89281779909@mail.ru>
2022-11-17 16:12:41 +0300

    использован шаблонизатор Jinja

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
  
 
 app = Flask(__name__)
@@ -6,12 +6,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return "<h1>Hello, world!</h1><br><a href='/index'>перейти на 2-ю страницу</a"
+    with open('file.txt', 'r', encoding='utf-8') as file:
+        resultData = list()
+        for line in file.readlines():
+            resultData.append(tuple(line.split('\n')[0].split(';')))
+
+    return render_template('base.html', data=resultData)
+    # "<h1>Hello, world!</h1><br><a href='/index'>перейти на 2-ю страницу</a"
 
 
-@app.route('/index/<x>/<y>')
-def index(x,y):
-    return f'Результат: {float(x) + float(y)}'    
+@app.route('/about')
+def about():
+    return render_template('about.html')    
 
 
 if __name__ == '__main__':
