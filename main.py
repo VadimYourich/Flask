7cbb228da0c9789fdf46801cc44345c65f24607e
vadim <89281779909@mail.ru>
2022-11-18 21:37:36 +0300

    используя flask-wtf создали формы регистрации и авторизации

--- main.py
+++ main.py
@@ -1,23 +1,47 @@
 from flask import Flask, render_template
+from LoginForm import Lf
+from AuthForm import AuthF
  
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hello hello world'
 
 
 @app.route('/')
 def main():
-    with open('file.txt', 'r', encoding='utf-8') as file:
-        resultData = list()
-        for line in file.readlines():
-            resultData.append(tuple(line.split('\n')[0].split(';')))
+    return render_template('base.html')
 
-    return render_template('base.html', data=resultData)
-    # "<h1>Hello, world!</h1><br><a href='/index'>перейти на 2-ю страницу</a"
+@app.route('/register', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def reg():
+    form = Lf()
+    if form.validate_on_submit():
+        if form.password_again.data != form.password.data:
+            return render_template('register.html', title='Регистрация', form=form, 
+                    message='Пароли не совпадают!!!')
+
+        with open('file.txt', 'a', encoding='utf-8') as file:
+            file.write(f'{form.name.data};{form.email.data};{form.password.data}\n')
+        
+        return render_template('register.html', message='Регистрация прошла успешно')
+
+    return render_template('register.html', title='Регистрация', form=form)    
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html')    
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = AuthF()
+    if form.validate_on_submit():
+        with open('file.txt', 'r', encoding='utf-8') as file:
+            data = ' '.join(file.readlines())
+
+        if form.email.data not in data:
+            return render_template('login.html', form=form, message='Вы не зарегистрированы')
+        else:
+            for i in data.split():
+                if form.email.data in i:
+                    if i.split(';')[-1] == form.password.data:
+                        return render_template('login.html', message='Вы успешно авторизовались')
+    return render_template('login.html', form=form)
 
 
 if __name__ == '__main__':
